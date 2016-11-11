436f3afecf196178cdcf0510378698079f9d3e3a
karolku <karolku@greeny.cs.tlu.ee>
2016-11-11 11:02:32 +0200

    2 kodutöö jätk

---
+++ data.php
@@ -0,0 +1,123 @@
+﻿<?php
+
+	require("functions.php");
+	
+	//kas on sisse loginud, kui ei ole, siis suunata login lehele
+	
+	if (!isset ($_SESSION["userId"])) {
+		
+		header("Location: login.php");
+		exit();
+	}
+	
+	//kas ?logout on aadressireal
+	if (isset($_GET["logout"])) {
+		
+		session_destroy ();
+		header ("Location: login.php");
+		exit();
+	}
+
+	//ei ole tühjad väljad, mida salvestada
+	if(isset($_POST["author"]) &&
+			isset($_POST["title"]) &&
+			!empty($_POST["author"]) &&
+			!empty($_POST["title"])
+		) {
+			Books($_POST["author"], $_POST["title"]);
+			
+		}
+	
+	$author = "";
+	$authorError = "";
+	$title = "";
+	$titleError = "";
+	
+	if (isset ($_POST["author"]) ) {
+	
+		if (empty ($_POST["author"]) ) { 
+			$authorError = "Palun täitke see väli!";
+		} else {
+			$author = $_POST["author"];
+		}
+	}
+	
+	
+	if (isset ($_POST["title"]) ) {
+	
+		if (empty ($_POST["title"]) ) { 
+			$titleError = "Palun täitke see väli!";
+		} else {
+			$title = $_POST["title"];
+		}
+	}
+	
+	$people = AllBooks();
+	
+	//echo "<pre>";
+	//var_dump($people);
+	//echo "</pre>";
+?>
+<h1>Andmed</h1>
+<p> 
+	Tere tulemast <?=$_SESSION["email"];?>!
+	<a href="?logout=1">Logi välja</a>
+</p>
+
+<form method="POST"> 
+<label>Raamatu autor</label><br>
+			
+		<input type="text" name="author" value="<?=$author;?>"> <?php echo $authorError;?> <br><br>
+	
+<label>Raamatu pealkiri</label><br>
+		
+		<input type="text" name="title" value="<?=$title;?>"> <?php echo $titleError;?> <br><br>
+	
+	<input type="submit" value="Salvesta">	
+</form>
+
+
+
+<h2>Loetud raamatud</h2>
+<?php
+
+	$html = "<table>";
+	
+		$html .= "<tr>";
+			$html .= "<th>ID</th>";
+			$html .= "<th>Raamatu autor</th>";
+			$html .= "<th>Raamatu pealkiri</th>";
+			$html .= "<th>Loetud</th>";
+		$html .= "</tr>";
+
+	foreach($people as $p) {
+		$html .= "<tr>";
+			$html .= "<td>".$p->id."</td>";
+			$html .= "<td>".$p->author."</td>";
+			$html .= "<td>".$p->title."</td>";
+			$html .= "<td>".$p->created."</td>";
+		$html .= "</tr>";	
+	}
+
+	$html .= "</table>";
+	echo $html;
+	
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ functions.php
@@ -0,0 +1,138 @@
+﻿<?php
+
+	require("../../config.php");
+	//alustan sessiooni, et saaks kasutada $_SESSION muutujaid
+	session_start();
+
+	//****************
+	//*****SIGNUP*****
+	//****************
+	
+	$database = "if16_karoku";
+	
+	function signup ($email, $password, $username, $gender) {
+		
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+	
+		$stmt = $mysqli->prepare("INSERT INTO user_sample (email, password) VALUES (?, ?)");
+		echo $mysqli->error;
+		
+		$stmt->bind_param("ss", $email, $password);
+		
+		if ($stmt->execute()) {
+			echo "salvestamine õnnestus";
+		} else {
+			echo "ERROR".$stmt->error;
+		}
+	}
+
+	
+	function login($email, $password) {
+		
+		$error = "";
+		
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+	
+		$stmt = $mysqli->prepare("SELECT id, email, password, created FROM user_sample WHERE email = ?");
+		
+		echo $mysqli->error;
+		
+		//asendan küsimärgi
+		$stmt->bind_param("s", $email);
+		
+		//määran tulpadele muutujad
+		$stmt->bind_result($id, $emailFromDatabase, $passwordFromDatabase, $created);
+		$stmt->execute();
+		
+		//küsin rea andmeid
+		if($stmt->fetch()) {
+			//oli rida
+			
+			//võrdlen paroole
+			$hash = hash ("sha512", $password);
+			if($hash == $passwordFromDatabase) {
+				echo "kasutaja ".$id." logis sisse";
+				
+				$_SESSION["userId"] = $id;
+				$_SESSION["email"] = $emailFromDatabase;
+				
+				//suunaks uuele lehele
+				header("Location: data.php");
+				exit();
+				
+			} else {
+				$error = "Parool on vale";
+			}
+			
+		} else {
+			//ei olnud
+			$error = "Sellise emailiga ".$email."kasutajat ei olnud";
+			
+		}
+		
+		return $error;
+		
+	}
+	
+	
+	function Books ($author, $title) {
+		
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+	
+		$stmt = $mysqli->prepare("INSERT INTO Books (author, title, user) VALUES (?, ?, ?)");
+		echo $mysqli->error;
+		
+		$stmt->bind_param("ssi", $author, $title, $_SESSION["userId"]);
+		
+		if ($stmt->execute()) {
+			echo "Salvestamine õnnestus";
+		} else {
+			echo "ERROR".$stmt->error;
+		}
+	}
+	
+	function AllBooks() {
+		
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+	
+		$stmt = $mysqli->prepare("SELECT id, author, title, created FROM Books WHERE user=?");
+		
+		echo $mysqli->error;
+		$stmt->bind_param("i",$_SESSION["userId"]);
+		$stmt->bind_result($id, $author, $title, $created);
+		$stmt->execute();
+		
+		//array("Karoliina", "Kullamaa")
+		$result = array();
+		
+		//seni kuni on üks rida andmeid saada (10 rida = 10 korda)
+		while ($stmt->fetch()) {
+			
+			$person = new StdClass();
+			$person->id = $id;
+			$person->author = $author;
+			$person->title = $title;
+			$person->created = $created;
+			
+			//echo $color."<br>";
+			array_push($result, $person);
+		}
+		
+		$stmt->close();
+		$mysqli->close();
+		
+		return $result;
+	}
+	
+	function cleanInput($input) {
+		
+		$input = trim($input);
+		$input = stripslashes($input);
+		$input = htmlspecialchars($input);
+		
+		return $input;
+		
+	}
+
+?>
+
