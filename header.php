1af6aefbcdbed4326b94dedc9af1db3ef4e0092f
tanelm <tanelmaasalu@gmail.com>
2017-01-09 20:05:21 +0200

    III kodutöö failid

---
+++ header.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<style>
+  header {
+    height: 40px;
+    background: grey;
+	text-align: center;
+	font-size: 30px;
+	font-family: Lucida Console;
+  }
+</style>
+<header>
+Maaklerivabad yyrikorterid
+</header>
+<head>
+	<title>Logi sisse voi loo kasutaja</title>
+	
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" 
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<img src="header.jpg" alt="Header" width="1500" height="200" align="center">
+</head>
+<head>
+<style>
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333;
+}
+
+li {
+    float: left;
+}
+
+li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+li a:hover {
+    background-color: #111;
+}
+</style>
+</head>
+<body>
+
+<ul>
+  <li><a class="active" href="data.php">Avaleht</a></li>
+  <li><a href="apartments.php">Otsi kuulutust</a></li>
+  <li><a href="insert_apartment.php">Sisesta kuulutus</a></li>
+  <li><a href="?logout=1">Logi välja</a></li>
+</ul>
+
+</body>
+</html>
---
+++ page/data.php
@@ -0,0 +1,36 @@
+<?php
+   require("../functions.php");
+   
+   //kas on sisseloginud, kui ei ole siis
+   //suunata login lehele
+   if (!isset ($_SESSION["userId"])) {
+	   
+	   //header("Location: login.php");
+	   
+	}
+   
+   //kas ?loguout on aadressireal
+   if (isset($_GET["logout"])) {
+	   
+	   session_destroy();
+	   
+	   header("Location: login.php");
+	   exit();
+	   
+   }  
+   $informationError="";
+   
+?>
+<html>
+
+    <head>
+	<?php require("../header.php"); ?>
+	Tere tulemast <a href="user.php"><?=$_SESSION["userEmail"];?>!</a>
+	<a href="?logout=1">Logi välja</a>
+</p>
+	<body>
+Siin lehel saad sisestada ning otsida maaklerivabasid üürikortereid.
+
+</body>
+</html>		
+
